72e8d7ebd7a600ac82d50dd1a3d11cd4a2b20b7c
Volodymyr Vynnyk <33934184+volodymyrvynnyk@users.noreply.github.com>
2020-01-11 00:08:07 +0200

    Event (#2)
    * add event table
    
    * add pageSize menu
    
    * css fix
    
    * Upgrated eventTable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,25 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {ItemService} from "./item/item.service";
-import {ItemCreateComponent} from "./item/item-create/item-create.component";
-import {ItemTableComponent} from "./item/item-table/item-table.component";
-import {ItemSortableDirective} from "./item/item-sortable.directive";
-import {UserSigninComponent} from "./user/user-signin/user-signin.component";
-import {UserSignupComponent} from "./user/user-signup/user-signup.component";
-import {UserUpdateComponent} from "./user/user-update/user-update.component";
-import {UserForgotPasswordComponent} from "./user/user-forgot-password/user-forgot-password.component";
-import {UserResetPasswordComponent} from "./user/user-reset-password/user-reset-password.component";
-import {RegistrationService} from "./user/services/registration-service.service";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {AngularFontAwesomeModule} from "angular-font-awesome";
-import {LoginService} from "./user/services/login.service";
-import {ResetPasswordService} from "./user/services/reset-password.service";
-import {JwtModule} from "@auth0/angular-jwt";
-import {GlobalErrorHandler} from "./errors/global-error-handler";
+import {ItemService} from './item/item.service';
+import {ItemCreateComponent} from './item/item-create/item-create.component';
+import {ItemTableComponent} from './item/item-table/item-table.component';
+import {ItemSortableDirective} from './item/item-sortable.directive';
+import {EventComponent} from './event/event.component';
+import {EventService} from './event/service/event.service';
+import {UserSigninComponent} from './user/user-signin/user-signin.component';
+import {UserSignupComponent} from './user/user-signup/user-signup.component';
+import {UserUpdateComponent} from './user/user-update/user-update.component';
+import {UserForgotPasswordComponent} from './user/user-forgot-password/user-forgot-password.component';
+import {UserResetPasswordComponent} from './user/user-reset-password/user-reset-password.component';
+import {RegistrationService} from './user/services/registration-service.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {LoginService} from './user/services/login.service';
+import {ResetPasswordService} from './user/services/reset-password.service';
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+import {JwtModule} from '@auth0/angular-jwt';
+import {GlobalErrorHandler} from './errors/global-error-handler';
 
 export function tokenGetter() {
   let jwtToken = '';
@@ -36,6 +39,7 @@ export function tokenGetter() {
     ItemCreateComponent,
     ItemTableComponent,
     ItemSortableDirective,
+    EventComponent,
     UserSigninComponent,
     UserSignupComponent,
     UserUpdateComponent,
@@ -50,6 +54,7 @@ export function tokenGetter() {
     NgbModule,
     AngularFontAwesomeModule,
     ReactiveFormsModule,
+    NgMultiSelectDropDownModule.forRoot(),
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
@@ -60,11 +65,12 @@ export function tokenGetter() {
     })
   ],
   providers: [
-    { provide: ErrorHandler, useClass: GlobalErrorHandler },
+    {provide: ErrorHandler, useClass: GlobalErrorHandler},
     ItemService,
     RegistrationService,
     LoginService,
-    ResetPasswordService
+    ResetPasswordService,
+    EventService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ItemTableComponent} from "./item/item-table/item-table.component";
 import {ItemCreateComponent} from "./item/item-create/item-create.component";
+import {EventComponent} from './event/event.component';
 import {UserSignupComponent} from "./user/user-signup/user-signup.component";
 import {UserUpdateComponent} from "./user/user-update/user-update.component";
 import {UserResetPasswordComponent} from "./user/user-reset-password/user-reset-password.component";
@@ -12,12 +13,12 @@ import {UserSigninComponent} from "./user/user-signin/user-signin.component";
 const routes: Routes = [
   {path: 'item-table', component: ItemTableComponent},
   {path: 'item-create', component: ItemCreateComponent},
+  {path: 'events', component: EventComponent},
   {path: 'user-signup', component: UserSignupComponent},
   {path: 'user-signin', component: UserSigninComponent},
   {path: 'user-update', component: UserUpdateComponent},
   {path: 'user-reset-password', component: UserResetPasswordComponent},
-  {path: 'user-forgot-password', component: UserForgotPasswordComponent},
-
+  {path: 'user-forgot-password', component: UserForgotPasswordComponent}
   ];
 
 
