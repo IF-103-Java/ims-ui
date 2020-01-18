6f43f8c921ef970dcced56a7252390fc10ea8665
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-18 23:45:01 +0200

    Change hardcoded url in registration-service.service.ts and reset-password.service.ts

--- src/app/user/services/reset-password.service.ts
+++ src/app/user/services/reset-password.service.ts
@@ -1,20 +1,20 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetPasswordService {
-  constructor(private http: HttpClient) {
+  constructor(@Inject('BASE_API_URL') private baseUrl: string, private http: HttpClient) {
   }
 
   public sendResetPasswordToken(email: string) {
     let headers = new HttpHeaders();
-    return this.http.post('http://localhost:8080/forgot-password/', email, {headers});
+    return this.http.post(this.baseUrl + '/forgot-password/', email, {headers});
   }
 
   public resetPassword(token: string, newPassword: string) {
     let headers = new HttpHeaders();
-    return this.http.post('http://localhost:8080/reset-password?emailUUID=' + token, newPassword, {headers});
+    return this.http.post(this.baseUrl + '/reset-password?emailUUID=' + token, newPassword, {headers});
   }
 }
