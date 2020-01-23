2e1a9405ea4c65522495abcf1809732f0de16297
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-23 07:33:09 +0200

    Rename main-screen.component to home.component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -9,15 +9,20 @@ import {UserResetPasswordComponent} from './user/user-reset-password/user-reset-
 import {UserForgotPasswordComponent} from './user/user-forgot-password/user-forgot-password.component';
 import {UserSigninComponent} from './user/user-signin/user-signin.component';
 import {ItemSortableDirective} from './item/item-sortable.directive';
-import {MainScreenComponent} from './main-screen/main-screen.component';
+import {HomeComponent} from './home/home.component';
 
 export const routerComponents = [
+  // main components("/home", "/sign-in", "/sign-up")
+  HomeComponent,
+  UserSigninComponent,
+  UserSignupComponent,
+  // nav components:
+  // "/home/(nav:users)",
+  // "/home/(nav:warehouses)"
   ItemCreateComponent,
   ItemTableComponent,
   ItemSortableDirective,
   EventComponent,
-  UserSigninComponent,
-  UserSignupComponent,
   UserUpdateComponent,
   UserForgotPasswordComponent,
   UserResetPasswordComponent
@@ -27,14 +32,14 @@ export const routerComponents = [
   imports: [
     RouterModule.forRoot([
       {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'sign-in', component: UserSigninComponent},
+      {path: 'sign-up', component: UserSignupComponent},
       {
-        path: 'home', component: MainScreenComponent, children: [
+        path: 'home', component: HomeComponent, children: [
           {path: 'events', component: EventComponent, outlet: 'nav'},
           {path: 'item-table', component: ItemTableComponent, outlet: 'nav'},
         ]
       },
-      {path: 'sign-in', component: UserSigninComponent},
-      {path: 'sign-up', component: UserSignupComponent}
     ])
   ],
   exports: [
