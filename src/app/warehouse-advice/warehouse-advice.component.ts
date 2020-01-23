417b4cc3cf4301fc45775a2784a06d7b31a5cb3d
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-23 22:32:47 +0200

    Generate warehouse-advice.component

---
+++ src/app/warehouse-advice/warehouse-advice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-advice',
+  templateUrl: './warehouse-advice.component.html',
+  styleUrls: ['./warehouse-advice.component.css']
+})
+export class WarehouseAdviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
