18d197f3f7acf49b80fd8eb2962b515384aacea0
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-23 07:12:08 +0200

    Add nav <router-outlet>

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {ItemTableComponent} from './item/item-table/item-table.component';
 import {ItemCreateComponent} from './item/item-create/item-create.component';
 import {EventComponent} from './event/event.component';
@@ -9,6 +9,7 @@ import {UserResetPasswordComponent} from './user/user-reset-password/user-reset-
 import {UserForgotPasswordComponent} from './user/user-forgot-password/user-forgot-password.component';
 import {UserSigninComponent} from './user/user-signin/user-signin.component';
 import {ItemSortableDirective} from './item/item-sortable.directive';
+import {MainScreenComponent} from './main-screen/main-screen.component';
 
 export const routerComponents = [
   ItemCreateComponent,
@@ -22,21 +23,19 @@ export const routerComponents = [
   UserResetPasswordComponent
 ];
 
-const routes: Routes = [
-  {path: 'item-table', component: ItemTableComponent},
-  {path: 'item-create', component: ItemCreateComponent},
-  {path: 'events', component: EventComponent},
-  {path: 'user-signup', component: UserSignupComponent},
-  {path: 'user-signin', component: UserSigninComponent},
-  {path: 'user-update', component: UserUpdateComponent},
-  {path: 'user-reset-password', component: UserResetPasswordComponent},
-  {path: 'user-forgot-password', component: UserForgotPasswordComponent}
-];
-
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {
+        path: 'home', component: MainScreenComponent, children: [
+          {path: 'events', component: EventComponent, outlet: 'nav'},
+          {path: 'item-table', component: ItemTableComponent, outlet: 'nav'},
+        ]
+      },
+      {path: 'sign-in', component: UserSigninComponent},
+      {path: 'sign-up', component: UserSignupComponent}
+    ])
   ],
   exports: [
     RouterModule
