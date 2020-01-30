49550a7a191fb5f78d864977c71df385e7d43cef
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-30 03:24:12 +0200

    Fix whitelistedDomains in environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://staging-if103java.pp.ua:8080',
-  whitelistedDomains: ['staging-if103java.pp.ua'],
+  whitelistedDomains: ['staging-if103java.pp.ua:8080'],
 };
