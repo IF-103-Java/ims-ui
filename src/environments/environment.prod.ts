b4b6b5b0432b136bb6900d98f14a3dad94acb22f
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-01-31 19:14:17 +0200

    Add websocketURL to environmets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://staging-if103java.pp.ua',
+  wsEndPoint: 'http://staging-if103java.pp.ua/ims-websocket',
   whitelistedDomains: ['staging-if103java.pp.ua'],
 };
