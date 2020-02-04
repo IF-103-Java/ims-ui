a082e7878bd154fcdc462a8305aaf2b7aaf62e26
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-02-04 22:06:41 +0200

    dropdaownlist

--- src/app/warehouse/warehouse-create/warehouse-create.component.ts
+++ src/app/warehouse/warehouse-create/warehouse-create.component.ts
@@ -4,6 +4,7 @@ import {WarehouseService} from '../service/warehouse.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Address} from '../../models/address';
 import {ToastService} from '../../websocket/notification/toast.service';
+import {IDropdownSettings} from "ng-multiselect-dropdown";
 
 @Component({
   selector: 'app-warehouse',
@@ -11,7 +12,19 @@ import {ToastService} from '../../websocket/notification/toast.service';
   styleUrls: ['./warehouse-create.component.css']
 })
 export class WarehouseCreateComponent implements OnInit {
+
+  dropdownSettings: IDropdownSettings = {
+
+    singleSelection: true,
+    itemsShowLimit: 1,
+    allowSearchFilter: true
+  };
+
   warehouse: Warehouse = new Warehouse();
+
+  topMap = new Map<string, number>();
+  topList = new Array<string>();
+  childrenList = new Map<string, number>();
 
   constructor(private warehouseService: WarehouseService,
               private router: Router,
@@ -23,6 +36,20 @@ export class WarehouseCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.warehouseService.getWarehousesPage(0, 100, 'id,Acs')
+      .subscribe(data => {
+        data.content.forEach((warehouse) => {
+          this.topMap.set(warehouse.name, warehouse.id);
+          this.topList.push(warehouse.name);
+          console.log(this.topList);
+        });
+      });
+  }
+
+  getParentsList() {
+    this.topList = new Array<string>();
+
+
   }
 
   onSubmit() {
